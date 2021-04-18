a29a142c99fa73268c9fd4bb471e8098014aee9a
Dexter Santucci <38226103+DexterLagan@users.noreply.github.com>
2021-04-18 16:51:17 +0200

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,4 +1,11 @@
 #lang info
+
+;;; purpose
+
+; The Cobalt 2 color theme for DrRacket - converted by Dexter Santucci
+
+;;; main 
+
 (define framework:color-schemes
   '(#hash((colors
            .
@@ -16,131 +23,4 @@
             (framework:default-text-color #(#xFF #xFF #xFF))))
           (name . "Cobalt 2"))))
 
-;; background
-;#x193549
-;
-;; caret
-;#xffc600
-;
-;; foreground
-;#xFFFFFF
-;
-;; invisibles
-;#x7f7f7fb2
-;
-;; line highlight
-;#x1f4662
-;
-;; selection
-;#x0050a4
-;
-;; brackets foreground
-;#xffc600
-;
-;; brackets content foreground
-;#x000000
-;
-;; constant
-;#xFF628C
-;
-;; entity
-;#xffc600
-;
-;; keyword
-;#xFF9D00
-;
-;; storage
-;#xffc600
-;
-;; string
-;#x3AD900
-;
-;; comment
-;#x0088FF
-;
-;; support
-;#x80FFBB
-;
-;; variable
-;; foreground:
-;#xCCCCCC
-;; background:
-;#x0d3a58
-;
-;; lang variable
-;#xFF80E1
-;
-;; function call
-;#xFFEE80
-;
-;; invalid
-;; foreground
-;#x800F00
-;; background
-;#xF8F8F8
-;
-;; regex
-;#x80FFC2
-;
-;; error
-;#xDA2000
-
-
-
-
-
-
-
-
-;color names:
-;(drracket:error-background-highlighting
-; drracket:language-name-and-memory-use-at-top-of-interactions
-; drracket:syncheck:document-identifier
-; drracket:syncheck:matching-identifiers
-; drracket:syncheck:tail-arrow
-; drracket:syncheck:template-arrow
-; drracket:syncheck:untacked
-; drracket:syncheck:unused-identifier
-; drracket:syncheck:var-arrow
-; framework:basic-canvas-background
-; framework:default-text-color
-; framework:disabled-background-color
-; framework:failed-search-background-color
-; framework:line-numbers
-; framework:line-numbers-current-line-number-background
-; framework:line-numbers-current-line-number-foreground
-; framework:line-numbers-when-word-wrapping
-; framework:misspelled-text-color
-; framework:paren-match-color
-; framework:program-contour-current-location-bar
-; framework:warning-background-color
-; stepper:arrow-color
-; stepper:error-color
-; stepper:redex-highlight-color
-; stepper:reduct-highlight-color)
-;
-;style names:
-;(drracket:check-syntax:both-obligation-style-pref
-; drracket:check-syntax:free-variable
-; drracket:check-syntax:imported
-; drracket:check-syntax:lexically-bound
-; drracket:check-syntax:my-obligation-style-pref
-; drracket:check-syntax:set!d
-; drracket:check-syntax:their-obligation-style-pref
-; drracket:check-syntax:unk-obligation-style-pref
-; drracket:check-syntax:unused-require
-; drracket:read-eval-print-loop:error-color
-; drracket:read-eval-print-loop:out-color
-; drracket:read-eval-print-loop:value-color
-; framework:syntax-color:scheme:comment
-; framework:syntax-color:scheme:constant
-; framework:syntax-color:scheme:error
-; framework:syntax-color:scheme:hash-colon-keyword
-; framework:syntax-color:scheme:keyword
-; framework:syntax-color:scheme:other
-; framework:syntax-color:scheme:parenthesis
-; framework:syntax-color:scheme:string
-; framework:syntax-color:scheme:symbol
-; framework:syntax-color:scheme:text
-; plt:module-language:test-coverage-off
-; plt:module-language:test-coverage-on)
+; EOF
